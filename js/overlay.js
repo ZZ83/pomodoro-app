a6b41ac6d92d804024749712a09e40105d2bf6ba
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2022-08-15 20:18:32 -0700

    Add functionality to show and hide overlay

---
+++ js/overlay.js
@@ -0,0 +1,10 @@
+const overlay = document.querySelector(".overlay");
+
+export function showOverlay() {
+    overlay.style.display = "block";
+}
+
+export function hideOverlay() {
+    overlay.style.display = "none";
+}
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+import {showOverlay, hideOverlay} from './overlay.js'
+
+
 //FUNCTIONS ------------------------------------------------------------------------
 function addClassTo(element, klass) {
     element.classList.add(klass);
@@ -13,13 +16,47 @@ function removeClassFromList(list, klass) {
     })
 };
 
-
 const cycleButtons = document.querySelectorAll(".cycle-btn");
 const cycleSection = document.querySelector(".cycle-section");
-
 cycleSection.addEventListener("click", (event) => {
     if(event.target.tagName === "BUTTON") {
         removeClassFromList(cycleButtons, "cycle-btn--active");
         addClassTo(event.target, "cycle-btn--active")
     }
 })
+
+
+
+
+
+
+
+
+let isOverlayVisible = false;
+
+const overlay            = document.querySelector(".overlay");
+const settingButton      = document.querySelector(".settings-btn");
+const overlayCloseButton = document.querySelector(".settings__close-btn");
+
+function closeOverlayIfVisible() {
+    if (isOverlayVisible) {
+        hideOverlay();
+        isOverlayVisible = false;
+    }
+}
+
+settingButton.addEventListener("click", () => {
+    showOverlay();
+    isOverlayVisible = true;
+})
+
+overlay.addEventListener("click", (event) => {
+    if(event.target.tagName === "DIV" &&  event.target.className === "overlay") {
+        closeOverlayIfVisible();
+    }
+})
+
+overlayCloseButton.addEventListener("click", () => {
+   closeOverlayIfVisible();
+})
+
